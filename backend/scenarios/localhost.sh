9246ac5d7f0ae5a05c6653ea824b5044d925c6d2
Daniele Venzano <venza@brownhat.org>
2014-01-07 14:32:32 +0100

    Add variable for host ip

--- backend/scenarios/localhost.sh
+++ backend/scenarios/localhost.sh
@@ -6,11 +6,13 @@ source scenarios/common.def
 
 OUT_DIR=$BASE_OUT_DIR/localhost
 
+HOST=127.0.0.1
+
 mkdir -p $OUT_DIR
 
 for p in $C_SEQ_JAIN; do
 	echo "Running with $p concurrency"
-	./osmef.py --out_dir $OUT_DIR -o json btc -d $D -n "Bigfoot1 localhost $D seconds, $p concurrency" -c$p $USER@192.168.46.10 $USER@192.168.46.10
+	./osmef.py --out_dir $OUT_DIR -o json btc -d $D -n "Bigfoot1 localhost $D seconds, $p concurrency" -c$p $USER@$HOST $USER@$HOST
 done
 
 
