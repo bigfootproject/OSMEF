f6b5603d4bab0e2668264708cff8444929349f34
Daniele Venzano <venza@brownhat.org>
2014-05-20 14:09:12 +0200

    Implement more scenarios and one experiment

---
+++ experiments/vms_on_same_host/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+PYTHONPATH=../..
+../../osmef.py -s local2 -d
+../../osmef.py -s local2 -d
+../../osmef.py -s local2 -d
+../../osmef.py -s local2 -d
+../../osmef.py -s local2 -d
+
+../../osmef.py -s local3 -d
+../../osmef.py -s local3 -d
+../../osmef.py -s local3 -d
+../../osmef.py -s local3 -d
+../../osmef.py -s local3 -d
