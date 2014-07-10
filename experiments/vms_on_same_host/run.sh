c54d81f0745c913a518aef3192d8ccf644443020
Daniele Venzano <venza@brownhat.org>
2014-07-10 14:11:13 +0200

    More results

--- experiments/vms_on_same_host/run.sh
+++ experiments/vms_on_same_host/run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 PYTHONPATH=../..
-for i in `seq 10`; do ../../osmef.py -s local21 -d; done
-for i in `seq 10`; do ../../osmef.py -s local22 -d; done
-for i in `seq 10`; do ../../osmef.py -s local23 -d; done
-for i in `seq 10`; do ../../osmef.py -s local24 -d; done
-for i in `seq 10`; do ../../osmef.py -s local25 -d; done
-for i in `seq 10`; do ../../osmef.py -s local26 -d; done
-for i in `seq 10`; do ../../osmef.py -s local27 -d; done
-for i in `seq 10`; do ../../osmef.py -s local28 -d; done
-for i in `seq 10`; do ../../osmef.py -s local29 -d; done
-for i in `seq 10`; do ../../osmef.py -s local30 -d; done
+for i in `seq 10`; do ../../osmef.py -s local31 -d; done
+for i in `seq 10`; do ../../osmef.py -s local32 -d; done
+for i in `seq 10`; do ../../osmef.py -s local33 -d; done
+for i in `seq 10`; do ../../osmef.py -s local34 -d; done
+for i in `seq 10`; do ../../osmef.py -s local35 -d; done
+for i in `seq 10`; do ../../osmef.py -s local36 -d; done
+for i in `seq 10`; do ../../osmef.py -s local37 -d; done
+for i in `seq 10`; do ../../osmef.py -s local38 -d; done
+for i in `seq 10`; do ../../osmef.py -s local39 -d; done
+for i in `seq 10`; do ../../osmef.py -s local40 -d; done
